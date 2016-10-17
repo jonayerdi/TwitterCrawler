b4ca494a55d06bae4f3dfc48d37af9d88e254b7b
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-10-17 12:38:03 +0200

    Crawler only looks for user and mention tweets

--- src/main/TwitterCrawler.java
+++ src/main/TwitterCrawler.java
@@ -19,7 +19,7 @@ public class TwitterCrawler {
     public static final String CREDENTIALS_FILE = "credentials.ini";
 
     public static final int MIN_RETWEETS = 1;
-    public static final int FETCH_TWEETS = 1000;
+    public static final int FETCH_TWEETS = 10000;
 
     public static final double MAX_SCORE = 1.0;
     public static final double MIN_SCORE = 0.7;
@@ -31,7 +31,7 @@ public class TwitterCrawler {
     public static void main(String[] args) {
         Console.captureOutput();
         TwitterCrawler crawler = new TwitterCrawler();
-        crawler.start2();
+        crawler.start3();
     }
 
     public void start() {
@@ -62,6 +62,43 @@ public class TwitterCrawler {
         }
     }
 
+    public void start3() {
+        try {
+            twitter = new TweetNavigator(CREDENTIALS_FILE);
+            Scanner in = new Scanner(System.in);
+            Console.out.print("TweetID: ");
+            long tweetID = Long.valueOf(in.nextLine());
+            List<ScoredTweet> result = crawlBestTweets(tweetID);
+            FileOutputStream out = new FileOutputStream(tweetID + ".csv");
+            ScoredTweet.writeToCSV(result,out);
+            out.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns a list with the extracted tweets that match best with the original tweet
+     *
+     * @param tweetID TweetID of the tweet to crawl
+     * @return A list with the extracted tweets that match best with the original tweet
+     */
+    public List<ScoredTweet> crawlBestTweets(long tweetID) {
+        return crawlBestTweets(new Tweet(twitter.getTweetByTweetID(tweetID)));
+    }
+
+    /**
+     * Returns a list with the extracted tweets that match best with the original tweet
+     *
+     * @param tweet Tweet to crawl
+     * @return A list with the extracted tweets that match best with the original tweet
+     */
+    public List<ScoredTweet> crawlBestTweets(Tweet tweet) {
+        List<Tweet> crawled = crawl(tweet);
+        List<ScoredTweet> filtered = getBestTweets(tweet, crawled);
+        return filtered;
+    }
+
     /**
      * Extracts tweets related to the provided tweet
      *
@@ -83,6 +120,8 @@ public class TwitterCrawler {
         int lastResultSize = 0;
         //Do not repeat users
         Set<String> crawledUsers = new HashSet<String>();
+        //Do not repeat mentions
+        Set<String> crawledMentions = new HashSet<String>();
         //We use Maps with textHashes as keys so that there are no duplicates
         Map<Integer,Tweet> toCrawlResponse = new HashMap<Integer,Tweet>();
         Map<Integer,Tweet> toCrawlUser = new HashMap<Integer,Tweet>();
@@ -111,9 +150,12 @@ public class TwitterCrawler {
                 //Early exit
                 if(result.size() + toCrawlResponse.size() + toCrawlUser.size() - 1 >= FETCH_TWEETS) break;
                 Tweet crawl = toCrawlResponse.get(key);
-                //List<Tweet> fetchList = Tweet.createList(twitter.getResponseTweets(crawl.getStatus(), MIN_RETWEETS, 100));
-                List<Tweet> fetchList = Tweet.createList(twitter.getMentionTweets(crawl.getStatus(), MIN_RETWEETS, 100));
-                fetchList.addAll(Tweet.createList(twitter.getInReplyToTweets(crawl.getStatus(), 100)));
+                List<Tweet> fetchList = new ArrayList<>();//Tweet.createList(twitter.getResponseTweets(crawl.getStatus(), MIN_RETWEETS, 100));
+                if(!crawledMentions.contains(crawl.getStatus().getUser().getScreenName())) {
+                    fetchList = Tweet.createList(twitter.getMentionTweets(crawl.getStatus(), MIN_RETWEETS, 100));
+                    crawledMentions.add(crawl.getStatus().getUser().getScreenName());
+                }
+                //fetchList.addAll(Tweet.createList(twitter.getInReplyToTweets(crawl.getStatus(), 100)));
                 for(Tweet fetchTweet : fetchList)
                     if(!crawledUsers.contains(fetchTweet.getStatus().getUser().getScreenName())) {
                         toCrawlUser.put(fetchTweet.getTextHash(),fetchTweet);
@@ -139,9 +181,21 @@ public class TwitterCrawler {
         return resultList;
     }
 
-    public List<ScoredTweet> getBestTweets(Tweet query, List<Tweet> tweets, double minScore, double maxScore) {
+    /**
+     * Returns the tweets that best match the query with their corresponding TFIDF score
+     *
+     * @param query The query tweet
+     * @param tweets The documents to score
+     * @param minScore Minimum score needed for a document to show in result
+     * @param maxScore Maximum score needed for a document to show in result
+     * @param minTerms Minimum number of terms a document needs in order to show in result
+     * @param minTermSize Minimum number of characters a term must have to count in the score
+     * @return The tweets that best match the query with their corresponding TFIDF score
+     */
+    public List<ScoredTweet> getBestTweets(Tweet query, List<Tweet> tweets
+            , double minScore, double maxScore, int minTerms, int minTermSize) {
         TfidfFilter filter = new TfidfFilter(TfidfFilter.ScoringMode.TFIDF
-                , new PorterStemmer(), MIN_TERMS, MIN_TERM_SIZE);
+                , new PorterStemmer(), minTerms, minTermSize);
         List<ScoredTweet> scored = filter.getScores(query, tweets);
         List<ScoredTweet> filtered = new ArrayList<>();
         for(ScoredTweet scoredTweet : scored)
@@ -150,8 +204,15 @@ public class TwitterCrawler {
         return filtered;
     }
 
+    /**
+     * Returns the tweets that best match the query with their corresponding TFIDF score
+     *
+     * @param query The query tweet
+     * @param tweets The documents to score
+     * @return The tweets that best match the query with their corresponding TFIDF score
+     */
     public List<ScoredTweet> getBestTweets(Tweet query, List<Tweet> tweets) {
-        return getBestTweets(query, tweets, MIN_SCORE, MAX_SCORE);
+        return getBestTweets(query, tweets, MIN_SCORE, MAX_SCORE, MIN_TERMS, MIN_TERM_SIZE);
     }
 
 }
